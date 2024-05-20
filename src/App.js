a5f90c7d4a56d3e572e04f32a8fd9a6d04e2bcea
LUCAS NICOLAS KOZIUPA <47572827@ort>
2024-05-20 09:02:41 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Inicio from './views/Inicio/index.jsx';
+import AcercaDeMi from './views/AcercaDeMi/index.jsx';
 
 function App() {
   return (
     <>
       <Inicio/>
+      <AcercaDeMi/>
     </>
   );
 }
