9e748e2e6c564703ff40d480ea22be3a66ceb915
LUCAS NICOLAS KOZIUPA <47572827@ort>
2024-05-23 12:06:27 -0300

    experiencia

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,6 +1,20 @@
-const Card = () =>{
+const Card = ({titulo, locacion, duracion, imagen, texto}) =>{
     return(
         <>
+            <div class="card mb-3">
+                <div class="card-header px-3 py-2">
+                    <div class="d-flex justify-content-between">
+                        <h3 class="h5 mb-1">{titulo}</h3>
+                        <div class="text-muted text-small">
+                            {locacion}<small>{duracion}</small>
+                        </div>
+                        <img src={imagen} width={48} height={48} alt="icono"></img>
+                    </div>
+                    <div class="card-body px-3 py-2">
+                        <p>{texto}</p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- src/views/Experiencia/index.jsx
+++ src/views/Experiencia/index.jsx
@@ -1,7 +1,30 @@
+import { Card } from "react-bootstrap"
+import imgApp from "../../images/app-development.svg";
+import imgUxUi from "../../images/ui-ux.svg";
+import imgWeb from "../../images/web-programming.svg";
+import imgAlgoritmo from "../../images/algorithm.svg";
 const Experiencia = () =>{
     return(
         <>
-        
+            <div class="container-narrow">
+                <div class="text-center mb-5">
+                    <h5 class="marker marker-center">Experiencia</h5>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <Card titulo = "Autos al Rio" locacion="Escuela ORT" duracion = "2023" imagen = {imgWeb} texto = ""/>
+                    </div>
+                    <div class="col-md-6">
+                        <Card titulo = "Save & Savor" locacion="Escuela ORT" duracion = "2024" imagen = {imgApp} texto = ""/>
+                    </div>
+                    <div class="col-md-6">
+                        <Card titulo = "UX/UI Challenge" locacion="Escuela ORT" duracion = "2023" imagen = {imgUxUi} texto = ""/>
+                    </div>
+                    <div class="col-md-6">
+                        <Card titulo = "Young Entrepreneurship Program" locacion="Escuela ORT" duracion = "2023" imagen = {imgAlgoritmo} texto = ""/>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
